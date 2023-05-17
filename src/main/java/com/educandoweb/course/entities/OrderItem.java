467597d8c2c0b22ddc205ed9f783722aac0504bc
faggian <felipesantosfaggian@gmail.com>
2023-05-17 08:11:52 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -40,7 +40,7 @@ public class OrderItem implements Serializable {
 		id.setOrder(order);
 	}
 
-	//@JsonIgnore
+	// @JsonIgnore
 	public Product getProduct() {
 		return id.getProduct();
 	}
@@ -63,6 +63,10 @@ public class OrderItem implements Serializable {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+
+	public Double getSubTotal() {
+		return price * quantity;
 	}
 
 	@Override
